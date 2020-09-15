ae55859cf1b447bed6090fbba227807b4fbe0a75
Nikolai Poverud <nikpov@intern.i-sone.no>
2020-09-15 14:07:12 +0200

    testing

---
+++ Reset-PasswordViaPureservice.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+    [string]$ticketNumber
+)
+
+$Version = "1.0"
+
+$versionCheck = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/ResetADPassword.ps1" -UseBasicParsing
+
+if($versioncheck -contains "`$Vers")
+
+
+
+if ($ticketNumber) {
+    .\ResetADPassword.ps1 -ticketnumber $ticketnumber
+}
+else {
+    .\ResetADPassword.ps1
+}
+
+
