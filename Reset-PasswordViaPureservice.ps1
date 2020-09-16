7746104a29ebdf7ee3d076ccf6aed519b1311b23
Nikolai Poverud <nikpov@intern.i-sone.no>
2020-09-16 09:56:05 +0200

    added some write-hosts

--- Reset-PasswordViaPureservice.ps1
+++ Reset-PasswordViaPureservice.ps1
@@ -6,8 +6,16 @@ $Version = 1.1
 
 $versionCheck = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/version.json" -UseBasicParsing
 
+Write-Host "Sjekker versjonsnummer... Du kjører versjon $Version..."
+Start-Sleep 2
 if ($Version -ne $versionCheck) {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/ResetADPassword.ps1" -OutFile "C:\Github\PureservicePasswordReset\ResetADPassword.ps1"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/ResetADPassword.ps1" -OutFile "C:\Pureservice\ResetADPassword.ps1"
+    Write-Host "Ny versjon er $Versioncheck... Oppdaterer scriptet"
+    Start-Sleep 2
+
+}
+else{
+    Write-Host "Du er allerede på nyeste versjon..."
 }
 
 $Path = "C:\Pureservice"
