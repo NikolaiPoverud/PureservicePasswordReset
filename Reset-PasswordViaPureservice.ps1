5c875c7dbebd52311008054c1e3ac3029257f3df
Nikolai Poverud <nikpov@intern.i-sone.no>
2020-09-23 15:41:00 +0200

    new test

--- Reset-PasswordViaPureservice.ps1
+++ Reset-PasswordViaPureservice.ps1
@@ -1,7 +1,15 @@
 ﻿Param(
-    [string]$ticketNumber
+    [string]$ticketNumber,
+    $WaitFor,
+    $Cleanup
 )
 
+if (!($Waitfor -eq $null)) {
+    Do {
+        $proc = Get-Process -Id $WaitFor
+    }
+    until ($proc -eq $null)
+}
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/ResetADPassword.ps1" -OutFile "C:\Pureservice\ResetADPassword.ps1" -UseBasicParsing
 
@@ -15,4 +23,6 @@ else {
     Start-Process "powershell.exe" -ArgumentList "-file $Path\ResetADPassword.ps1"
 }
 
-
+if($Cleanup -eq $true) {
+    Remove-Item -Path "$Path\Reset-PasswordViaPureservice.ps1" -Force
+}
--- ResetADPassword.ps1
+++ ResetADPassword.ps1
@@ -37,13 +37,13 @@ if ($Version -eq $versionCheck) {
 else {
     Write-Host "Ny versjon er $Versioncheck... Oppdaterer scriptet"
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/NikolaiPoverud/PureservicePasswordReset/master/Reset-PasswordViaPureservice.ps1" -OutFile "C:\Pureservice\Reset-PasswordViaPureservice.ps1" -UseBasicParsing
-    Start-Process "powershell.exe" -ArgumentList "-File `"C:\Pureservice\Reset-PasswordViaPureservice.ps1`" -ticketnumber $ticketnumber -Wait Pid"  
+    Start-Process "powershell.exe" -ArgumentList "-File `"C:\Pureservice\Reset-PasswordViaPureservice.ps1`" -ticketnumber $ticketnumber -WaitFor $pid -Cleanup $true"  
+    Start-Sleep 4
     break
 }
 
 
 if ($ticketnumber) {
-
     
     $baseUri = $Config.baseUri 
     $apiKey = $config.apiKey
@@ -119,7 +119,7 @@ else {
     $userObj = Get-ADUser -Filter { SamAccountName -eq $UserName } -Properties mail
     $userName = $userObj.SamAccountName
     $enabled = $userObj.enabled
-    $mail = $userObj.mail
+
 
     if ($enabled -eq $false) {
         Unlock-ADAccount -Identity $userObj
